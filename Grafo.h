b21631a66396a71944fccec57d3d0ee288c59cc4
Italo Nunes Tillmann de Abreu <italontab@hotmail.com>
2025-03-16 11:45:21 -0300

    feat - Implementação completa do código para o trabalho de estrutura de dados 2 com todas as funções implementadas e usecase na main() do grafo soc-tribes

--- Grafo.h
+++ Grafo.h
@@ -5,16 +5,19 @@ typedef struct grafo Grafo;
 Grafo *cria_Grafo(int limite_de_vertices, int eh_ponderado);
 void libera_Grafo(Grafo *G);
 
+int insereVertice(Grafo *G, char *label);
+
 // u = origem, v = destino e w = peso se for ponderado
 int insereAresta(Grafo *G, int u, int v, float w);
-
-int insereVertice(Grafo *G, char *label);
-int removeAresta(Grafo *G, int v);
+int removeAresta(Grafo *G, int u, int v);
 int grauMedio(Grafo *G);
 int grauMax(Grafo *G, int *v);
 int grau(Grafo *G, int v);
 
 int getLimiteVertices(Grafo *G);
 int getNumeroVertices(Grafo *G);
-
+int contaArestas(Grafo *G);
+int verificaConexaBfs(Grafo *G, int start, int *visitado);
+int validaConexas(Grafo *G, int *tamMax);
+int validaMenorCaminhoBfs(Grafo *G, int start, int *dist);
 #endif
